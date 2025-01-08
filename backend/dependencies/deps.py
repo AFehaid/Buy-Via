0a70e6171ca98951964960e98388553af56e15f5
AWahiby <146235160+AWahiby@users.noreply.github.com>
2025-01-08 17:23:52 +0300

    Enhanced Login and AuthProvider and SearchResults

--- backend/dependencies/deps.py
+++ backend/dependencies/deps.py
@@ -1,7 +1,7 @@
 # Buy-Via\backend\dependencies\deps.py
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import jwt, JWTError
@@ -28,8 +28,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 # Authentication Dependency
-async def get_current_user(token: str = Depends(oauth2_bearer)):
+async def get_current_user(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
     try:
+        # Remove "Bearer " prefix if present
+        if token.startswith("Bearer "):
+            token = token[7:]
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
